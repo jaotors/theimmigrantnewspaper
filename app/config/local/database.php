29fc06eb9e464247ac01d0041dce8918fe60d97b
jaotors <turingan.joshua@gmail.com>
2015-05-07 10:33:14 +0800

    update some shizz

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'silver',
+			'database'  => 'theimmigrant',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
